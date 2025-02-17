e31d3d4597e697473bdeb97f508acfe75b9f922e
Biagiarelli Adriano <adrianobiagiarelli@gmail.com>
2025-02-17 09:48:52 +0100

    Add cloudinary image storage set up

--- gamebits/backend/gamebits/settings.py
+++ gamebits/backend/gamebits/settings.py
@@ -36,7 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
 
     'rest_framework',
     'rest_framework_simplejwt',
