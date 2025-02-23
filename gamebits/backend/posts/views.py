dc8eea6e14082cae8e25403b70f6081388a9c4b7
Biagiarelli Adriano <adrianobiagiarelli@gmail.com>
2025-02-23 10:54:16 +0100

    Add filter posts liked and followed

--- gamebits/backend/posts/views.py
+++ gamebits/backend/posts/views.py
@@ -1,14 +1,33 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from posts.models import Post
 from api.serializers import PostSerializer
+from likes.models import Like
+from follow.models import Follow
 
 # Create your views here.
 class PostListCreateView(generics.ListCreateAPIView):
     """View to list and create posts."""
-    queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        """Allow filtering by liked and followed posts."""
+        user = self.request.user
+        queryset = Post.objects.all().order_by('-created_at')
+
+        filter_type = self.request.query_params.get('filter')
+
+        if user.is_authenticated:
+            if filter_type == 'liked':
+                liked_posts = Like.objects.filter(user=user).values_list('post_id', flat=True)
+                queryset = queryset.filter(id__in=liked_posts)
+            
+            elif filter_type == 'followed':
+                followed_users = Follow.objects.filter(follower=user).values_list('following_id', flat=True)
+                queryset = queryset.filter(author__id__in=followed_users)
+        
+        return queryset
 
     def perform_create(self, serializer):
         """Override perform_create to associate the post with the logged-in user."""
@@ -16,10 +35,9 @@ class PostListCreateView(generics.ListCreateAPIView):
 
 class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
     """View to retrieve, update, or delete a single post."""
-    queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         """Ensure users can only edit or delete their own posts."""
-        return Post.objects.filter(author=self.request.user)
+        return Post.objects.all()
