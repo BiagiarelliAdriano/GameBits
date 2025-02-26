4d5e6aff08a6aa37ab0383fe206f8b2cccea65be
Biagiarelli Adriano <adrianobiagiarelli@gmail.com>
2025-02-26 19:08:12 +0100

    Adjust ALLOWED_HOSTS in settings.py

--- gamebits/backend/gamebits/settings.py
+++ gamebits/backend/gamebits/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'gamebits.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'https://gamebits-579c6fd85599.herokuapp.com/']
 
 
 # Application definition
