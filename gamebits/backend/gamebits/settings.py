0fb9eba7d548392803db3c3dd39153cfb9b74dc8
Biagiarelli Adriano <adrianobiagiarelli@gmail.com>
2025-02-27 20:26:31 +0100

    Add heroku allowed hosts

--- gamebits/backend/gamebits/settings.py
+++ gamebits/backend/gamebits/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'https://gamebits-579c6fd85599.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*.herokuapp.com']
 
 
 # Application definition
