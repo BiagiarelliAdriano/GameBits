a15d671ed858f87bf85bb3aa0afebb09444b4b3a
Biagiarelli Adriano <adrianobiagiarelli@gmail.com>
2025-03-08 11:11:40 +0100

    Change order of custom apps in installed apps for testing

--- gamebits/backend/gamebits/settings.py
+++ gamebits/backend/gamebits/settings.py
@@ -64,13 +64,13 @@ INSTALLED_APPS = [
     'dj_rest_auth',
     'dj_rest_auth.registration',
 
-    # 'users',
-    'posts',
-    'likes',
     'comments',
-    'notifications',
     'follow',
+    'likes',
+    'notifications',
+    'posts',
     'replies',
+    'users',
 ]
 
 MIDDLEWARE = [
